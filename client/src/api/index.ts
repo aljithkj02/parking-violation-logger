9250547db18ac9b84dc5c479ce430ed5bfaddabd
Aljith K J <kwdeathlok@gmail.com>
2025-04-23 10:28:31 +0530

    Add axios instance

---
+++ client/src/api/index.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL as string;
+
+export const api = axios.create({
+    baseURL: API_URL,
+    withCredentials: true,
+});
+
+// Request Interceptor
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access_token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// Response Interceptor
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("access_token");
+        }
+
+        return Promise.reject(error);
+    }
+);
+
