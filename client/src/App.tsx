f2598b0a49db29768c8ae7afcd45d08ab7e0e147
Aljith K J <kwdeathlok@gmail.com>
2025-04-23 10:34:55 +0530

    add toast and singup, login api functions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom"
 import AppRouter from "./routes/AppRouter"
+import { Toaster } from "react-hot-toast"
 
 
 function App() {
 
     return (
         <BrowserRouter>
+            <Toaster />
             <AppRouter />
         </BrowserRouter>
     )
---
+++ client/src/api/auth.ts
@@ -0,0 +1,49 @@
+
+import { toast } from "react-hot-toast";
+import { api } from "./axios";
+
+export interface SignupPayload {
+    name: string;
+    email: string;
+    password: string;
+    role: "ADMIN" | "USER";
+}
+
+export interface LoginPayload {
+    email: string;
+    password: string;
+    role: "ADMIN" | "USER";
+}
+
+export interface AuthResponse {
+    message: string;
+    token: string;
+    user: {
+        id: string;
+        name: string;
+        email: string;
+        role: "ADMIN" | "USER";
+    };
+}
+
+export const signup = async (data: SignupPayload): Promise<AuthResponse | null> => {
+    try {
+        const res = await api.post("/auth/signup", data);
+        toast.success("Signup successful!");
+        return res.data;
+    } catch (error: any) {
+        toast.error(error?.response?.data?.message || "Signup failed");
+        return null;
+    }
+};
+
+export const login = async (data: LoginPayload): Promise<AuthResponse | null> => {
+    try {
+        const res = await api.post("/auth/login", data);
+        toast.success("Login successful!");
+        return res.data;
+    } catch (error: any) {
+        toast.error(error?.response?.data?.message || "Login failed");
+        return null;
+    }
+};
