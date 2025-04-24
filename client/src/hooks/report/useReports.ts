97535342907b700997b235d0884444a28a054522
Aljith K J <kwdeathlok@gmail.com>
2025-04-24 12:15:17 +0530

    Add filter for reports

--- client/src/hooks/report/useReports.ts
+++ client/src/hooks/report/useReports.ts
@@ -4,12 +4,12 @@ import { useAppSelector } from "../useAppSelector";
 import { fetchReports } from "../../store/slices/reportSlice";
 import { ReportFilter } from "../../utils/report.type";
 
-export const useReports = (page: number, limit: number, filter?: ReportFilter) => {
+export const useReports = (page: number, limit: number, filter?: ReportFilter | 'all') => {
     const dispatch = useAppDispatch();
     const reportState = useAppSelector((state) => state.report);
 
     useEffect(() => {
-        dispatch(fetchReports({ page, limit, filter }));
+        dispatch(fetchReports({ page, limit, filter: filter === 'all' ? undefined: filter}));
     }, [page, limit, filter, reportState.refetch]);
 
     return { ...reportState };
--- client/src/pages/Reports.tsx
+++ client/src/pages/Reports.tsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 import { useReports } from "../hooks/report/useReports";
 import { ReportCard } from "../components/report/ReportCard";
 import { ReportModal } from "../components/report/ReportModal";
-import { Report } from "../utils/report.type";
+import { Report, ReportFilter } from "../utils/report.type";
 import { useAppDispatch } from "../hooks/useAppDispatch";
 import { refetchData } from "../store/slices/reportSlice";
 
 export const Reports = () => {
     const [page, setPage] = useState(1);
     const [limit] = useState(5);
+    const [filter, setFilter] = useState<ReportFilter | 'all'>('all');
     const [selectedReport, setSelectedReport] = useState<Report | null>(null);
 
-    const { data, loading, error, totalPages } = useReports(page, limit);
+    const { data, loading, error, totalPages } = useReports(page, limit, filter);
 
     const dispatch = useAppDispatch();
 
@@ -19,11 +20,40 @@ export const Reports = () => {
 
     const isAdmin = user.role === "ADMIN";
 
+    const filterOptions = [
+        { value: 'all', label: 'All Reports' },
+        { value: 'ACTION_TAKEN', label: 'Action Taken' },
+        { value: 'UNDER_REVIEW', label: 'Under Review' },
+    ];
+
+    const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const newFilter = event.target.value as ReportFilter;
+        setFilter(newFilter);
+    };
+
     return (
         <div className="px-4">
             <h1 className="text-2xl font-bold text-center mb-6 text-gray-800">
                 {isAdmin ? "Citizen Reports" : "My Reports"}
             </h1>
+
+            <div className="mb-4 flex justify-end">
+                <div>
+                    <select
+                        value={filter}
+                        onChange={handleFilterChange}
+                        className="block sm:w-40 mt-1 px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all ease-in-out"
+                    >
+                        {filterOptions.map((option) => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+
+
             {loading ? (
                 <p className="text-center">Loading...</p>
             ) : error ? (
